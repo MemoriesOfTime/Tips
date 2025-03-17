f2a88e2aba1897892884a2e4371a1c32f1dbb9c2
LT_Name <572413378@qq.com>
2025-03-17 20:54:14 +0800

    fix: TipTask memory problem

--- src/main/java/tip/tasks/TipTask.java
+++ src/main/java/tip/tasks/TipTask.java
@@ -2,17 +2,20 @@ package tip.tasks;
 
 import cn.nukkit.Player;
 import cn.nukkit.Server;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import tip.Main;
 import tip.utils.SendPlayerClass;
-import java.util.LinkedHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 若水
  */
 public class TipTask extends BaseTipsRunnable {
 
-
-    private static final LinkedHashMap<Player, SendPlayerClass> sendPlayerClassLinkedHashMap = new LinkedHashMap<>();
+    private static final Cache<Player, SendPlayerClass> sendPlayerClassCache = CacheBuilder.newBuilder()
+            .expireAfterAccess(1, TimeUnit.MINUTES)
+            .build();
 
     public TipTask(Main owner, int sleep) {
         super(owner);
@@ -28,10 +31,12 @@ public class TipTask extends BaseTipsRunnable {
         while (this.owner.isEnabled()) {
             for (Player player : Server.getInstance().getOnlinePlayers().values()) {
                 try {
-                    if (!sendPlayerClassLinkedHashMap.containsKey(player)) {
-                        sendPlayerClassLinkedHashMap.put(player, new SendPlayerClass(player, getOwner()));
+                    SendPlayerClass sendPlayerClass;
+                    if ((sendPlayerClass = sendPlayerClassCache.getIfPresent(player)) == null) {
+                        sendPlayerClass = new SendPlayerClass(player, getOwner());
+                        sendPlayerClassCache.put(player, sendPlayerClass);
                     }
-                    sendPlayerClassLinkedHashMap.get(player).init();
+                    sendPlayerClass.init();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
